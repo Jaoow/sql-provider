626fe77bca00a7f6b8f2dc667fc1d913adc5dc51
joao-py <xfx.joao@gmail.com>
2021-12-04 17:08:09 -0300

    🔧 Set SQLite dependency to compile only.

--- connector/build.gradle.kts
+++ connector/build.gradle.kts
@@ -7,8 +7,8 @@ dependencies {
     val slf4jVersion = "1.7.32"
     val sqliteVersion = "3.36.0.2"
 
+    compileOnly("org.xerial:sqlite-jdbc:${sqliteVersion}")
     implementation("com.zaxxer:HikariCP:$hikariVersion")
     implementation("org.slf4j:slf4j-api:$slf4jVersion")
     implementation("org.slf4j:slf4j-simple:$slf4jVersion")
-    implementation("org.xerial:sqlite-jdbc:${sqliteVersion}")
 }
