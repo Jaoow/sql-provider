fc9b3ae5908cbec1219bca49c32e9f5f114e8bd4
joao-py <xfx.joao@gmail.com>
2021-12-04 17:09:51 -0300

    ✍️ Reduce AdapterNotFoundException class.

--- executor/src/main/java/com/jaoow/sql/executor/exceptions/SQLAdapterNotFoundException.java
+++ executor/src/main/java/com/jaoow/sql/executor/exceptions/SQLAdapterNotFoundException.java
@@ -9,28 +9,11 @@ import com.jaoow.sql.executor.SQLExecutor;
 public class SQLAdapterNotFoundException extends SQLExecutorException {
 
     /**
-     * Create an exception without cause
-     */
-    public SQLAdapterNotFoundException() {
-        super();
-    }
-
-    /**
-     * Create an exception with cause
-     *
-     * @param message the cause
-     */
-    public SQLAdapterNotFoundException(String message) {
-        super(message);
-    }
-
-    /**
      * Create an exception with name of invalid class
      *
      * @param clazz the invalid class
      */
     public SQLAdapterNotFoundException(Class<?> clazz) {
-        super("the adapter for class " + clazz.getSimpleName() + " was not found.");
+        super("The adapter for class " + clazz.getSimpleName() + " was not found.");
     }
-
 }
