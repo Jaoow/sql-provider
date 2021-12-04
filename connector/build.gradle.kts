2aff21daf1eea77441637e8b426f8a7a46be8cd9
joao-py <xfx.joao@gmail.com>
2021-12-04 17:07:50 -0300

    ⬆️ Update SQLite version.

--- connector/build.gradle.kts
+++ connector/build.gradle.kts
@@ -5,7 +5,7 @@ repositories {
 dependencies {
     val hikariVersion = "3.4.5"
     val slf4jVersion = "1.7.32"
-    val sqliteVersion = "3.25.2"
+    val sqliteVersion = "3.36.0.2"
 
     implementation("com.zaxxer:HikariCP:$hikariVersion")
     implementation("org.slf4j:slf4j-api:$slf4jVersion")
