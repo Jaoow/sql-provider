cfd2443e9d3ba9d31ce1ad5054d5769c37b4dae5
SrBlecaute01 <josearnaldoma18k@gmail.com>
2022-07-31 00:54:56 -0300

    :bug: Fixing SQLException in SQLResultAdapter.

--- executor/src/main/java/com/jaoow/sql/executor/adapter/SQLResultAdapter.java
+++ executor/src/main/java/com/jaoow/sql/executor/adapter/SQLResultAdapter.java
@@ -1,10 +1,15 @@
 package com.jaoow.sql.executor.adapter;
 
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
 import java.sql.ResultSet;
+import java.sql.SQLException;
 
 @FunctionalInterface
 public interface SQLResultAdapter<T> {
 
-    T adaptResult(ResultSet record);
+    @Nullable
+    T adaptResult(@NotNull ResultSet record) throws SQLException;
 
 }
